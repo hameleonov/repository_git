66dfa3a969bc24493d5d5602c838944f4ce215b7
hameleonov <hameleonov@gmail.com>
2012-10-19 00:42:13 +0200

    knjkn

---
+++ src/dog.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xeops
+ * Date: 19.10.12
+ * Time: 1:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class dog {
+
+    private String say="GAB";
+    int y=5;
+}

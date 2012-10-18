c9afa0e264d99415d264fc3528f01c810c7b30fc
hameleonov <hameleonov@gmail.com>
2012-10-19 01:05:22 +0200

    hfhghtthr

---
+++ src/bird.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xeops
+ * Date: 19.10.12
+ * Time: 1:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class bird {
+    private String say="Crazy";
+    int y=7;
+}
---
+++ src/olololol.js
@@ -0,0 +1,8 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xeops
+ * Date: 19.10.12
+ * Time: 1:57
+ * To change this template use File | Settings | File Templates.
+ */
+sdfgdsfg

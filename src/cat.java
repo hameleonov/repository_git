2f78cf998dc54e1fa5bc917086fba3f1a8287956
hameleonov <hameleonov@gmail.com>
2012-10-19 00:38:12 +0200

    knjkn

---
+++ src/cat.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xeops
+ * Date: 19.10.12
+ * Time: 1:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class cat {
+
+    private String say="My9";
+    int y=4;
+}

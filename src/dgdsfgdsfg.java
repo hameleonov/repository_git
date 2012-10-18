a36f6941b1d083a18693c52254fc8f5957208ce4
hameleonov <hameleonov@gmail.com>
2012-10-18 21:51:22 +0200

    dghfghdsfgdfgd

---
+++ src/dgdsfgdsfg.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xeops
+ * Date: 18.10.12
+ * Time: 22:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class dgdsfgdsfg {
+}
---
+++ src/sdfgds.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xeops
+ * Date: 18.10.12
+ * Time: 22:48
+ * To change this template use File | Settings | File Templates.
+ */
